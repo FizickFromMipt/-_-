46a863ac3c793eeb657fab265a94663e6dc24ad8
alex <alex.pevnenko@gmail.com>
2024-02-13 13:42:14 +0300

    add code

--- test/src/main/java/ru/pevnenko/Example4.java
+++ test/src/main/java/ru/pevnenko/Example4.java
@@ -7,7 +7,9 @@ import java.util.concurrent.CopyOnWriteArrayList;
  * Исходный класс из тестового задания
  *
  * Какие вижу проблемы :
- * -
+ * - Consumer удаляет первое вхождение элемента i (методом remove)
+ * - после первой итерации потребитель не удаляет ничего, так как все елементы уже удалены
+ * - проблема с многопоточостью, так как я пытаюсь тут изменяь общую коллекцию  из нескольких потоков без синхронизации
  */
 public class Example4 {
 
@@ -58,3 +60,31 @@ public class Example4 {
 
     }
 }
+
+/**
+ * Вывод:
+ *
+ *produces size = 1
+ * consumer size = 1
+ * produces size = 2
+ * produces size = 2
+ * consumer size = 1
+ * consumer size = 2
+ * consumer size = 1
+ * consumer size = 1
+ * consumer size = 1
+ * produces size = 3
+ * produces size = 2
+ * produces size = 3
+ * produces size = 4
+ * produces size = 5
+ * produces size = 6
+ * consumer size = 1
+ * produces size = 7
+ * consumer size = 6
+ * consumer size = 5
+ * consumer size = 4
+ * final size = 4
+ *
+ * Process finished with exit code 0
+ */
--- test/src/main/java/ru/pevnenko/Example4New.java
+++ test/src/main/java/ru/pevnenko/Example4New.java
@@ -1,5 +1,6 @@
 package ru.pevnenko;
 
+import java.util.Collections;
 import java.util.concurrent.CopyOnWriteArrayList;
 
 
@@ -27,15 +28,15 @@ public class Example4New {
         @Override
         public void run() {
             for (int i = 0; i < 10; i++) {
-                ls.remove(Integer.valueOf(i));
+                ls.removeAll(Collections.singleton(i));
                 System.out.println("consumer size = " + ls.size());
             }
             System.out.println("final size = " + ls.size());
         }
     }
 
-    public static void main(String[] args) throws InterruptedException {
 
+    public static void main(String[] args) throws InterruptedException {
         Example4New example = new Example4New();
 
         Example4New.Producer produces = example.new Producer();
@@ -45,8 +46,36 @@ public class Example4New {
         Thread consumerThread = new Thread(consumer);
 
         producerThread.start();
-
         consumerThread.start();
 
     }
 }
+
+/**
+ * Вывод :
+ *
+ * produces size = 1
+ * produces size = 2
+ * produces size = 3
+ * produces size = 4
+ * produces size = 5
+ * produces size = 6
+ * produces size = 7
+ * produces size = 8
+ * produces size = 9
+ * produces size = 10
+ * consumer size = 9
+ * consumer size = 8
+ * consumer size = 7
+ * consumer size = 6
+ * consumer size = 5
+ * consumer size = 4
+ * consumer size = 3
+ * consumer size = 2
+ * consumer size = 1
+ * consumer size = 0
+ * final size = 0
+ *
+ * Process finished with exit code 0
+ *
+ */
